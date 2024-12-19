9e26d038d6c30c494a36148e6cb6c577d0b18f0f
akashrawr <akashkm553@gmail.com>
2024-12-19 13:05:33 +1200

    interaction objects

---
+++ src/main/java/org/akashdev/object/OBJ_Chest.java
@@ -0,0 +1,19 @@
+package org.akashdev.object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Chest extends SuperObject {
+
+    public OBJ_Chest(){
+
+        name = "chest";
+
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/chest.png"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
