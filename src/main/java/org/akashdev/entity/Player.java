e634d3e3c56ce52c2fc5178fa3884bb5ba26d727
akashrawr <akashkm553@gmail.com>
2024-12-23 20:20:33 +1200

    added monsters

--- src/main/java/org/akashdev/entity/Player.java
+++ src/main/java/org/akashdev/entity/Player.java
@@ -41,6 +41,8 @@ public class Player extends Entity {
 
         worldX= gp.tileSize * 23;
         worldY= gp.tileSize * 21;
+        //worldX= gp.tileSize * 10;
+        //worldY= gp.tileSize * 13;
         speed=4;
         direction = "down";
 
@@ -91,6 +93,10 @@ public class Player extends Entity {
             int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
             interactNPC(npcIndex);
 
+            // CHECK MONSTER COLLISION
+            int monsterIndex = gp.cChecker.checkEntity(this, gp.monster);
+            contactMonster(monsterIndex);
+
             // CHECK EVENT
             gp.eHandler.checkEvent();
 
@@ -124,6 +130,14 @@ public class Player extends Entity {
                 }
             }
         }
+
+        if(invincible == true){
+            invincibleCounter++;
+            if(invincibleCounter > 60){
+                invincible = false;
+                invincibleCounter = 0;
+            }
+        }
     }
 
     public void pickUpObject(int i){
@@ -142,6 +156,16 @@ public class Player extends Entity {
             }
         }
     }
+
+    public void contactMonster(int i){
+        if(i != 999){
+            if(invincible == false){
+                life -= 1;
+                invincible = true;
+            }
+        }
+    }
+
     public void draw(Graphics2D g2){
 
         //g2.setColor(Color.white);
@@ -184,7 +208,20 @@ public class Player extends Entity {
                 break;
         }
 
+        if(invincible == true){
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
+        }
         g2.drawImage(image, screenX, screenY,null);
         //g2.drawRect(screenX + solidArea.x, screenY + solidArea.y, solidArea.width, solidArea.height);
+
+        // RESET ALPHA
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+
+        // DEBUG
+        /*
+        g2.setFont(new Font("Times New Roman", Font.PLAIN, 26));
+        g2.setColor(Color.WHITE);
+        g2.drawString("Invincible: " + invincibleCounter, 10, 400);
+         */
     }
 }
